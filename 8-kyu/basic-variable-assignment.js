e7d3ff697a47068110b751a88f6d6328b5d57e13
July Moss <july.x.moss@gmail.com>
2022-03-13 15:41:44 +0000

    c added some more 8-kyu katas

---
+++ 8-kyu/basic-variable-assignment.js
@@ -0,0 +1,14 @@
+/*
+    This code should store "codewa.rs" 
+    as a variable called name but it's not working. 
+
+    var a == "code";
+    var b == "wa.rs";
+    var name == a + b;
+    
+    Can you figure out why?
+*/
+
+const a = "code";
+const b = "wa.rs";
+const name = a + b;
